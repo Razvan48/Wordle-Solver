5bbf6d08d9a198b083b9644255fd56295a9754e7
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2022-10-29 20:38:48 +0300

    Added the logic of solver.py

--- Solver/solver.py
+++ Solver/solver.py
@@ -34,21 +34,56 @@ def getBestWord(words):
 
 def readWords(address, words):
     words.clear()
-    file = open(address, "r")
+    file = open(address, 'r')
     fileline = file.readline()
     while fileline:
         words.append(fileline)
         fileline = file.readline()
+    file.close()
+
     index = 0
     while index < len(words):
         words[index] = words[index][:-1]
         index += 1
 
 
+def deleteUnwantedWords(words, feedback, word):              #TO DO
+    if feedback == '':
+        return;
+
+    # TO DO
+
+
 if __name__ == '__main__':
+    file = open('../gameMode.txt', 'w')
+    file.write('1')
+    file.close()
+
     words = []
-    readWords('../currentDataBase.txt', words)
-    print(getBestWord(words))
+    readWords('../dataBase.txt', words)
+
+    feedback = ''
+
+    while feedback != 'VVVVV':
+        deleteUnwantedWords(words, feedback, bestWord)
+
+        bestWord = getBestWord(words)
+        file = open('../currentWord.txt', 'w')
+        file.write(bestWord)
+        file.close()
+
+        exec('../Wordle/wordle.py')
+
+        file = open('../feedback.txt', 'r')
+        feedback = file.read()
+        file.close()
+
+    file = open('../gameMode.txt', 'w')
+    file.write('0')
+    file.close()
+
+    print(bestWord)
+
 
 
 
